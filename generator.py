f4864cbf4d5d4981689374876d8197d56254a749
elf <49889930+elfq@users.noreply.github.com>
2021-02-27 19:00:16 -0500

    remove "generate" from every method

--- generator.py
+++ generator.py
@@ -3,20 +3,17 @@ import random
 
 class string:
 
-  def generate_uppercase_int(size, chars=string.ascii_uppercase + string.digits):
+  def uppercase(size, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-  def generate_lowercase_int(size, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
-
-  def generate_lowercase(size, chars=string.ascii_lowercase + string.digits):
+  def lowercase(size, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
   
-  def generate_int(size, chars=string.digits):
+  def int(size, chars=string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
   
-  def generate_str_uppercase(size, chars=string.ascii_uppercase):
+  def str_uppercase(size, chars=string.ascii_uppercase):
     return ''.join(random.choice(chars) for _ in range(size))
 
-  def generate_str_lowercase(size, chars=string.ascii_lowercase):
+  def str_lowercase(size, chars=string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))

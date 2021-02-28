7f4cc8c2b75432a801c46c8414ea21e76e4036d9
elf <49889930+elfq@users.noreply.github.com>
2021-02-27 19:33:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Generator",
+    name="StringGen",
     version="0.0.1",
     author="elf",
     author_email="elflanded@gmail.com",
